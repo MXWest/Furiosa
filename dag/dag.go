a7fd76230854310ebb3ecd311a02ba3fa71968e8
MXWest <mwest@urbn.com>
2017-08-20 18:13:56 -0400

    Iss #3 Add rudimentary POC Server
    - Add rudimentary client
    - Add placeholder for dashboard

---
+++ dag/dag.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dag reports about repositories")
+	/*
+	We want dag to (I think) take an argument:
+	- org/Repositories
+	- desired output format and location. Let's start with GFM and a file
+	- Uses GitHub GraphQL https://developer.github.com/v4/
+	 */
+}
